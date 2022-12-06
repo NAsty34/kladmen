08b65ae864eb53cc6cc80443b1b25af2e4277ae6
NAsty34 <99412848+NAsty34@users.noreply.github.com>
2022-12-06 15:55:18 +0300

    one

---
+++ kladmen/Sklad_show.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace kladmen
+{
+    /// <summary>
+    /// Логика взаимодействия для Sklad.xaml
+    /// </summary>
+    public partial class Sklad_show : Window
+    {
+        public Sklad_show()
+        {
+            InitializeComponent();
+            list_sklad.ItemsSource = Entities.Mod().Sklad.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            new Main().Show();
+            Close();
+        }
+    }
+}
---
+++ kladmen/Vhod.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kladmen
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Vhod : Window
+    {
+        public Vhod()
+        {
+            InitializeComponent();
+            kod.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (kod.Text == "0000")
+            {
+                new Main().Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Вы вели неправильный пароль, введите 0000");
+            }
+        }
+    }
+}
